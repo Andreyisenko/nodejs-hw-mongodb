54812c99e24ee092a0f9e7792b4df1bb9e39d9cb
Andrey Isenko <aisenko87@gmail.com>
2025-03-29 13:58:55 +0200

    add mongo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import express from 'express';
-const PORT = 3000;
+import setupServer from './server.js';
 
-const app = express();
-// const sum = 10;
-// const message = 'Hello BoB Sinkler';
-// console.log(message);
-// console.log(sum);
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello BoB Sinkler',
-    });
-  });
-
-app.listen(PORT, () => {
-  console.log(`Servers running on port ${PORT}`);
-});
+setupServer();
--- src/server.js
+++ src/server.js
@@ -1,4 +1,35 @@
-// const setupServer = ()=> {
-    
-// };
-// console.log(setupServer);
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+function setupServer() {
+  const PORT = 3000;
+
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello BoB Sinkler',
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+  app.listen(PORT, () => {
+    console.log(`Servers running on port ${PORT}`);
+  });
+}
+
+export default setupServer;
