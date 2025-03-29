cdb8c399b02420f6d58e6e7077c0000d235c3043
Andrey Isenko <aisenko87@gmail.com>
2025-03-29 20:16:31 +0200

    add getcontacts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import setupServer from './server.js';
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,24 +1,46 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-function setupServer() {
-  const PORT = 3000;
+// import ContactCollection from './db/models/contact.js';
+import { getContacts, getContactsById } from './services/contacts.js';
+import { getEnvVar } from './utils/getEnvVar.js';
 
+export const setupServer = () => {
   const app = express();
-
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
   app.use(cors());
+  app.use(express.json());
+
+  //   app.use(
+  //     pino({
+  //       transport: {
+  //         target: 'pino-pretty',
+  //       },
+  //     }),
+  //   );
 
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello BoB Sinkler',
+    });
+  });
+
+  app.get('/api/contacts', async (req, res) => {
+    const data = await getContacts();
+    res.json({
+      status: 200,
+      message: 'Successfully find contacts',
+      data,
+    });
+  });
+
+  app.get('/api/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+
+    const data = await getContactsById(contactId);
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data,
     });
   });
 
@@ -27,9 +49,8 @@ function setupServer() {
       message: 'Not found',
     });
   });
-  app.listen(PORT, () => {
-    console.log(`Servers running on port ${PORT}`);
-  });
-}
+  const port = Number(getEnvVar('PORT', 3000));
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+};
 
-export default setupServer;
+// export default setupServer;
