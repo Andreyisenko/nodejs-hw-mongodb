d2b5902d5bd3ba0f35154e85877368f049b58904
Andrey Isenko <aisenko87@gmail.com>
2025-03-30 20:05:12 +0300

    add finisch

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,14 +4,15 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConnection = async () => {
   try {
-    const password = getEnvVar("MONGODB_PASSWORD");  
-    const user = getEnvVar("MONGODB_USER");
-    const url = getEnvVar("MONGODB_URL");
-    const name = getEnvVar("MONGODB_DB");
-    await mongoose.connect(`mongodb+srv://${user}:${password}@${url}/${name}?retryWrites=true&w=majority&appName=Cluster0`);
-console.log("Mongo connection successfully established!");
-  
-} catch (error) {
+    const password = getEnvVar('MONGODB_PASSWORD');
+    const user = getEnvVar('MONGODB_USER');
+    const url = getEnvVar('MONGODB_URL');
+    const name = getEnvVar('MONGODB_DB');
+    await mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/${name}?retryWrites=true&w=majority&appName=Cluster0`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
     console.log(error.message);
     throw error;
   }
