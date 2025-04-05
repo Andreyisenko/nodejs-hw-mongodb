805a7374107f2ceb5932d721323870519e97f380
Andrey Isenko <aisenko87@gmail.com>
2025-04-05 22:55:31 +0300

    add put

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,6 +3,8 @@ import { ctrlWrapper } from '../utils/trrlWrapper.js';
 import {
   getContactsController,
   getContactsByIdController,
+  addContactsController,
+  upsertContactsController,
 } from '../controllers/contacts.js';
 
 const contactsRouter = Router();
@@ -10,5 +12,9 @@ const contactsRouter = Router();
 contactsRouter.get('/', ctrlWrapper(getContactsController));
 
 contactsRouter.get('/:contactId', ctrlWrapper(getContactsByIdController));
+
+contactsRouter.post('/', ctrlWrapper(addContactsController));
+
+contactsRouter.put('/:contactId', ctrlWrapper(upsertContactsController));
 
 export default contactsRouter;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,3 +3,12 @@ import ContactCollection from '../db/models/contact.js';
 export const getContacts = () => ContactCollection.find();
 
 export const getContactsById = (id) => ContactCollection.findOne({ _id: id });
+
+export const addContact = (payload) => ContactCollection.create(payload);
+
+export const updateContact = async (_id, payload) => {
+  const data = await ContactCollection.findOneAndUpdate({_id}, payload);
+  
+  return data;
+
+};
