8a09b7b932e046090c98690900a737fb5a64cc40
Andrey Isenko <aisenko87@gmail.com>
2025-04-18 08:42:10 +0300

    add login

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../utils/validateBody.js';
-import { registerController } from '../controllers/auth.js';
-import { authRegisterSchema } from '../validation/auth.js';
+import { registerController, loginController } from '../controllers/auth.js';
+import { authRegisterSchema, authLoginSchema } from '../validation/auth.js';
 const authRouter = Router();
 
 authRouter.post(
   '/register',
-  // validateBody(authRegisterSchema),
+  validateBody(authRegisterSchema),
   ctrlWrapper(registerController),
+);
+
+authRouter.post(
+  '/login',
+  validateBody(authLoginSchema),
+  ctrlWrapper(loginController),
 );
 
 export default authRouter;
